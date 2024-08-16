c907f0a29687888b76bebb7586c97bfd72180697
Lololove3110 <jahir.hinojosa@gmail.com>
2024-08-16 11:36:04 -0600

    Agregue comentarios

--- semana1/src/juegoDeRolPolimorfismo2/ComportamientoCombate.java
+++ semana1/src/juegoDeRolPolimorfismo2/ComportamientoCombate.java
@@ -1,7 +1,21 @@
 package juegoDeRolPolimorfismo2;
 
+/* aqui se define los diferentes comportamientos del personaje y el enemigo, todavia falta sobreescribir 
+ * todos los tipos de comportamiento de los ataques
+ * Se muestran en las clases Shuriken, Hechizo, Espada y Llamarada
+ * 
+ */
 public interface ComportamientoCombate {
 	void EjecutaCombate(Personaje enemigo);
-	
 
 }
+
+/*Ejemplo de como se sobre escribe cada clase
+* public class Espada implements ComportamientoCombate {
+*  @Override
+*   public void EjecutaCombate(Personaje enemigo) {
+*     System.out.println("El Guerrero lanza una estocada con la espada!");  se declara el ataque
+*      enemigo.recibirDanio(20); se hace el daño pero no acepta java ñ
+*   }
+*}
+/*
